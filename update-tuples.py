3670a4294cf3a4415a8e6b9de1b64f84a4af2080
José Lescano <92216756+jlesca@users.noreply.github.com>
2021-10-27 09:44:35 -0300

    Update update-tuples.py

--- update-tuples.py
+++ update-tuples.py
@@ -13,3 +13,31 @@ colors[1] = 'white' # Modifico la posición 1, en este caso 'green' por 'white'.
 colors = tuple(colors) # Convierto nuevamente la var de lista a tupla.
 
 print(colors) # Muestro la var actualizada.
+
+
+# AGREGAR UN ITEM A UNA TUPLA
+# Como vimos las tuplas no pueden modificarse ni ser alteradas.
+# Por eso para agregar un elemento a una tupla existente, debemos convertirla en una lista para poder modificarla.
+# Una vez que agregamos el o los items, podremos reconvertirla en tupla.
+
+print('AGREGAR UN ITEM A LA TUPLA') # Muestro tìtulo.
+colors = ('red', 'green', 'blue') # Creo la tupla y sus items.
+print(colors) # Muestri la tupla.
+colors = list(colors) # Convierto la tupla en una lista.
+colors.append('yellow') # Agrego el item 'yellow' a la lista, mediante append()
+colors = tuple(colors) # Vuelvo a convertir la lista en una tupla.
+print(colors) # Muetro la tupla actualizada.
+
+
+# ELIMINAR UN ITEM DE LA TUPLA
+# Del mismo modo podemos proceder para eliminar un elemento.
+
+print('ELIMINAR UN ITEM DE LA TUPLA') # Muestro tìtulo.
+colors = ('red', 'green', 'blue') # Creo la tupla y sus items.
+print(colors) # Muestri la tupla.
+colors = list(colors) # Convierto la tupla en una lista.
+colors.remove('green') # Elimino el item 'green' a la lista, mediante remove()
+colors = tuple(colors) # Vuelvo a convertir la lista en una tupla.
+print(colors) # Muetro la tupla actualizada.
+
+input() # Salgo del programa.
