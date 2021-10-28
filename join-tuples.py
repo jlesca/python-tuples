b550b1e2d96ab39e0c1ecf5bde191cd334ef68a6
José Lescano <92216756+jlesca@users.noreply.github.com>
2021-10-28 14:53:58 -0300

    Create join-tuples.py

---
+++ join-tuples.py
@@ -0,0 +1,18 @@
+# UNIR TUPLAS
+# Podemos unir dos o más tuplas mediante el uso del operador '+'
+
+print('UNIR TUPLAS')
+colors = ('red','green', 'blue')
+fruits = ('apple', 'orange', 'cherry')
+result = colors + fruits
+print(result)
+
+
+# MULTIPLICAR ITEMS DE LA TUPLA
+# También podemos multiplicar el contenido de una tupla cuantas veces querramos.
+# Lo realizamos mediante el operador '*'
+
+print('MULTIPLICAR ITEMS EN UNA TUPLA')
+colors = ('red','green', 'blue')
+more_colors = colors * 2
+print(more_colors)
